132161164dd88c67492c528a0428708ee979f7f2
rangerboy574 <jdeweese574@gmail.com>
2023-01-29 11:33:51 -0600

    Initial Commit

---
+++ BuckJaw Login Screen Biometrics Template/BuckJaw_Login_Screen_Biometrics_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuckJaw_Login_Screen_Biometrics_TemplateApp.swift
+//  BuckJaw Login Screen Biometrics Template
+//
+//  Created by Joseph  DeWeese on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct BuckJaw_Login_Screen_Biometrics_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuckJaw Login Screen Biometrics Template/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BuckJaw Login Screen Biometrics Template
+//
+//  Created by Joseph  DeWeese on 1/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
